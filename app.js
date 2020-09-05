3ce0281d72171cba7bede4685617427d32aaa899
name <gramkracker88@gmail.com>
2020-09-05 13:56:35 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// CSS
+
+const random = document.querySelector('.random');
+
+// random.style.backgroundColor ='blue';
+// random.style.color ='white';
+// random.style.fontSize ='3rem';
+// random.style.textTransform ='capitalized';
+
+random.classList.add('title'); // adds the class title to random
